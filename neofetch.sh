b9c3b160e14eb06773626deeb8d7a1bc74dd2f87
dasp1983 <83188092+dasp1983@users.noreply.github.com>
2021-05-11 17:29:52 +0200

    Delete neofetch.sh

--- neofetch.sh
+++
@@ -1,4 +0,0 @@
- #!/bin/bash
- 
- # Adds neofetch to the motd.
- bash -c $'echo "echo" >> /etc/profile.d/mymotd.sh && echo "neofetch" >> /etc/profile.d/mymotd.sh && chmod +x /etc/profile.d/mymotd.sh'
